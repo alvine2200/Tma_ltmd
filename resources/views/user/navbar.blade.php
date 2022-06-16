6e0035583a717265a9de0873d44c5f2b89b86779
alvine2200 <Alvinellavu@gmail.com>
2022-06-16 16:02:10 +0300

    adds

--- resources/views/user/navbar.blade.php
+++ resources/views/user/navbar.blade.php
@@ -9,7 +9,7 @@
 
 	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/manyatta.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/manyatta.css')}}">
 
 </head>
 <body>

af81a26090a4ba0f2a0a3b2ddffb6250236b4137
alvine2200 <Alvinellavu@gmail.com>
2022-06-22 15:45:36 +0300

    new adds

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -31,6 +31,60 @@
         .inputBox{
             padding-top: 2rem;
         }
+        .maps-container{
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+            padding:30px;
+        }
+        .maps-container .row{
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+            margin: 20px;
+        }
+         .maps-container .row .header span{
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 30px;
+            color:darkslategray;
+            font-weight: bold;
+            padding-right: 30px;
+        }
+         .maps-container .row .map{
+            display:flex;
+            flex-wrap: wrap;
+            align-items: center;
+            justify-content: center;
+            object-fit: contain;          
+        }
+       
+
+        @media(max-width:445px){
+            .maps-container .row .header span{
+                display:flex;
+                align-items: center;
+                justify-content: center;
+               
+            }
+             .maps-container .row .map{
+                display:flex;
+                flex-wrap: wrap;
+                align-items: center;
+                justify-content: center;
+                margin: 20px;
+            }
+
+             .maps-container .row .map iframe {
+                display:flex;
+                flex-wrap: wrap;
+                width: 300px;
+            }
+
+        }
     </style>
 </head>
 <body>
@@ -71,11 +125,29 @@
                 <span>your message</span>
                 <textarea name="message" placeholder="enter your message" id="" cols="30" rows="10"></textarea>
             </div>
+            <div class="input">
+                <span>Comments(extra comments)</span>
+                <textarea name="message" placeholder="enter your comments" id="" cols="30" rows="10"></textarea>
+            </div>
           </div>
-          <input type="submit" value="Submit" name="" class="btn">
-
+          <input type="submit" value="Submit" name="" class="btn">          
         </form>
     </section>
+
+    <div class="maps-container">
+        <div class="row">
+            <div class="header">
+                <span>Githurai 45<br>KAMUKABI HOUSE<br>Ground Floor</span>
+            </div>
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.932318446871!2d36.922637114274394!3d-1.2074980358847034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f3fc4d99e244d%3A0xa56a71dd86c694b4!2sManyatta%20Films%20LTD!5e0!3m2!1sen!2ske!4v1655889868062!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+
+    
+    
+     
 
     @include('user.footer');
 
